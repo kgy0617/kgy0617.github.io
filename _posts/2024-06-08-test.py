da46081102caad33c64b314f672d8e4aefbe1397
kgy0617 <35395723+kgy0617@users.noreply.github.com>
2025-11-14 12:04:14 +0100

    Remove 2024-06-03 1st files

---
+++ _posts/2024-06-08-test.py
@@ -0,0 +1,9 @@
+user_input = input("enter a number: ")
+print(user_input)
+
+print(f"type: {type(user_input)} / value : {user_input}")
+
+
+
+num = input("enter a number :")
+print(f"type: {type(num)} / value: {num} ")
---
+++ _posts/2025-06-08-test1.py
@@ -0,0 +1,3 @@
+num = int(input("enter a number :"))
+
+print(f"type: {type(num)} / value: {num} ")
